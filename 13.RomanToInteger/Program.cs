2e4ab32fae7500f57b2e7298b144b5a824359371
chrome-dome <chrome-domepl@protonmail.com>
2023-02-05 08:53:03 +0100

    add task 13

---
+++ 13.RomanToInteger/Program.cs
@@ -0,0 +1,31 @@
+﻿//Given a roman numeral, convert it to an integer.
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class RomanToInteger
+{
+    static Dictionary<char, int> RomanValues = new Dictionary<char, int>()
+{
+{'I', 1},
+{'V', 5},
+{'X', 10},
+{'L', 50},
+{'C', 100},
+{'D', 500},
+{'M', 1000}
+};
+
+static int RomanToInt(string s)
+    {
+        return s.Select((c, i) => i > 0 && RomanValues[c] > RomanValues[s[i - 1]] ? RomanValues[c] - 2 * RomanValues[s[i - 1]] : RomanValues[c]).Sum();
+    }
+
+    static void Main()
+    {
+        string s = "XII";
+        Console.WriteLine(RomanToInt(s));
+        Console.ReadLine();
+    }
+}
