fb8d1a8cdcb88560d216fca71757cd4198abd122
chrome-dome <chrome-domepl@protonmail.com>
2023-02-07 19:53:40 +0100

    add task 26

---
+++ 26.Remove_Duplicates_from_Sorted_Array/Program.cs
@@ -0,0 +1,9 @@
+﻿//Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+
+int[] nums = new int[] { 1, 1, 2 };
+nums = nums.Distinct().ToArray();
+int newLength = nums.Length;
+Console.WriteLine(newLength); // output: 2
+Console.WriteLine(string.Join(",", nums)); // output: 1,2
+Console.ReadLine();
