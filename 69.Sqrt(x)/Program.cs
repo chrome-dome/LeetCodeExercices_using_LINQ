af56611e026af6f83683f5392ab1f7f33a420965
chrome-dome <chrome-domepl@protonmail.com>
2023-02-08 19:17:41 +0100

    add task 69

---
+++ 69.Sqrt(x)/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+//You must not use any built-in exponent function or operator.
+
+int squareRoot(int x)
+{
+    return (int)Math.Sqrt(x);
+}
+
+Console.WriteLine(squareRoot(4)); // Output: 2
+Console.WriteLine(squareRoot(9)); // Output: 3
+Console.WriteLine(squareRoot(16)); // Output: 4
+
+Console.ReadLine();
