37c48cf60a6fd7da3d72860f68190f0d342b43f4
chrome-dome <chrome-domepl@protonmail.com>
2023-02-09 19:53:02 +0100

    add task 70

---
+++ 70.ClimbingStairs/Program.cs
@@ -0,0 +1,23 @@
+﻿//You are climbing a staircase. It takes n steps to reach the top.
+
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+//!!!This problem is a classic example of dynamic programming and can be solved using the Fibonacci sequence!!!
+
+
+int ClimbStairs(int n)
+{
+    if (n <= 2) return n;
+
+return Enumerable.Range(3, n - 2)
+    .Aggregate(new List<int> { 1, 2 }, (acc, i) => {
+        acc.Add(acc[i - 3] + acc[i - 2]);
+        return acc;
+    }).Last();
+}
+
+int steps = ClimbStairs(2);
+Console.WriteLine(steps); // Output: 2
+
+Console.ReadLine();
