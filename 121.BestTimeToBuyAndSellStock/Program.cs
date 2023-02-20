8031d7bd1bfb88e8e2087dd202bad8755486dabd
chrome-dome <chrome-domepl@protonmail.com>
2023-02-20 11:28:42 +0100

    add task 121

---
+++ 121.BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+-You are given an array prices where prices[i] is the price of a given stock on the ith day.
+-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ */
+
+
+using System;
+using System.Linq;
+
+int MaxProfit(int[] prices)
+{
+    int minPrice = prices.Min();
+    int maxProfit = prices.Zip(prices.Skip(1), (x, y) => y - x).Max();
+    return maxProfit;
+}
+
+int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+int maxProfit = MaxProfit(prices);
+Console.WriteLine(maxProfit);
+Console.ReadLine();
