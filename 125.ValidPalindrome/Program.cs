0cf1fd2f4ebf63650a17daaa62a75ba2ff075f95
chrome-dome <chrome-domepl@protonmail.com>
2023-02-22 18:22:51 +0100

    add task 125

---
+++ 125.ValidPalindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+-A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+-Given a string s, return true if it is a palindrome, or false otherwise.
+ */
+
+using System;
+using System.Linq;
+
+static void Main(string[] args)
+{
+    Console.WriteLine(PalindromeChecker.IsPalindrome("A man, a plan, a canal: Panama"));
+    Console.ReadLine();
+}
+
+public class PalindromeChecker
+{
+    public static bool IsPalindrome(string s)
+    {
+        // Convert string to lowercase
+        s = s.ToLower();
+
+        // Remove non-alphanumeric characters
+    s = new string(s.Where(c => char.IsLetterOrDigit(c)).ToArray());
+
+        // Check for palindrome
+        return s.SequenceEqual(s.Reverse());
+    }
+}
