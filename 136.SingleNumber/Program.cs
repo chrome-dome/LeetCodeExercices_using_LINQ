674814345bc99040753cdf1895a9539eb87f04a8
chrome-dome <chrome-domepl@protonmail.com>
2023-02-22 18:24:29 +0100

    add task 136

---
+++ 136.SingleNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+-Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+-You must implement a solution with a linear runtime complexity and use only constant extra space.
+ */
+
+int SingleNumber(int[] nums)
+{
+    return nums.Aggregate((a, b) => a ^ b);
+}
+
+int[] nums = { 2, 2, 1 };
+int single = SingleNumber(nums);
+Console.WriteLine(single); // Output: 1
+Console.ReadLine();
