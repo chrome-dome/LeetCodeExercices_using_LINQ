579d350955822a6e247491875c30145603b036b6
chrome-dome <chrome-domepl@protonmail.com>
2023-02-25 22:36:15 +0100

    add task 217

---
+++ 217.ContainsDuplicate/Program.cs
@@ -0,0 +1,21 @@
+﻿//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static bool ContainsDuplicate(int[] nums)
+    {
+        return nums.GroupBy(x => x).Any(g => g.Count() > 1);
+    }
+
+static void Main(string[] args)
+    {
+        int[] nums = new int[] { 1, 2, 3, 1 };
+        bool result = ContainsDuplicate(nums);
+        Console.WriteLine(result); // output: true
+
+        Console.ReadLine();
+    }
+}
