4a3ee3d35673cfcba11abe20705c60b74ab1189e
chrome-dome <chrome-domepl@protonmail.com>
2023-02-28 22:44:13 +0100

    add task 283

---
+++ 283.MoveZeroes/Program.cs
@@ -0,0 +1,18 @@
+﻿//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+
+using System;
+using System.Linq;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 0, 1, 0, 3, 12 };
+        nums = nums.Where(x => x != 0).Concat(nums.Where(x => x == 0)).ToArray();
+
+        Console.WriteLine(string.Join(",", nums));
+        Console.ReadLine();
+    }
+}
+
