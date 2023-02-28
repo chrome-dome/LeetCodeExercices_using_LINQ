4ea36a7c1459a0625fa49bb8a2dfeb0707e6f1fd
chrome-dome <chrome-domepl@protonmail.com>
2023-02-28 22:43:02 +0100

    add task 268

---
+++ 268.MissingNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+int FindMissingNumber(int[] nums)
+{
+    int n = nums.Length;
+    int expectedSum = (n * (n + 1)) / 2;
+    int actualSum = nums.Sum();
+    return expectedSum - actualSum;
+}
+
+int[] nums = { 3, 0, 1 };
+int missingNum = FindMissingNumber(nums);
+Console.WriteLine(missingNum); // Output: 2
+Console.ReadLine();
