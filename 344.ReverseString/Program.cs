ca9b7fce3defe85f6c41441c4c1ab1e15ca480d6
chrome-dome <chrome-domepl@protonmail.com>
2023-03-02 12:10:38 +0100

    add task 344

---
+++ 344.ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿//Write a function that reverses a string. The input string is given as an array of characters s.
+
+//You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+//Version1 - 0(n)
+
+char[] ReverseString(char[] s)
+{
+    return s.Reverse().ToArray();
+}
+
+char[] s = { 'h', 'e', 'l', 'l', 'o' };
+Console.WriteLine(ReverseString(s));
+
+Console.ReadLine();
+
