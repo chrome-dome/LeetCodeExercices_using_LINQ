4353e37215c68927bb037a8bc8a4ffc6246528f0
chrome-dome <chrome-domepl@protonmail.com>
2023-03-06 17:15:06 +0100

    add task 387

---
+++ 387.FirstUniqueCharacterInAString/Program.cs
@@ -0,0 +1,31 @@
+﻿//Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+using System;
+using System.Linq;
+
+class MainClass
+{
+    public static char? FirstNonRepeatingChar(string s)
+    {
+        var nonRepeatingChar = s.GroupBy(c => c)
+        .Where(g => g.Count() == 1)
+        .Select(g => g.Key)
+        .FirstOrDefault();
+        return nonRepeatingChar;
+    }
+
+public static void Main(string[] args)
+    {
+        string s = "leetcode";
+        var index = s.IndexOf((char)FirstNonRepeatingChar(s));
+        if (index == -1)
+        {
+            Console.WriteLine("There is no non-repeating character in the string.");
+        }
+        else
+        {
+            Console.WriteLine("The first non-repeating character is at index " + index);
+        }
+        Console.ReadLine();
+    }
+}
